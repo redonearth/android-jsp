fac99bdc6ac4966a25fb0f58961f0fb5fa0f0e0e
redonearth <wona23@gmail.com>
2019-06-21 15:43:10 +0900

    Update Controller files

--- src/main/java/com/redonearth/androidjsp/controller/AndroidController.java
+++ src/main/java/com/redonearth/androidjsp/controller/AndroidController.java
@@ -6,22 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AndroidController {
 
-    @RequestMapping(value="/user-register")
-    public String UserRegister() {
-        return "UserRegister";
-    }
-
-    @RequestMapping(value="/user-validate")
-    public String UserValidate() {
-        return "UserValidate";
-    }
-
-    @RequestMapping(value="/user-login")
-    public String UserLogin() {
-        return "UserLogin";
-    }
-
-    @RequestMapping(value="/hello")
+    @RequestMapping("/hello")
     public String HelloSpringBoot() {
         return "hello";
     }
