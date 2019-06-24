192e75a77c1bca70104c2eb02db6db35367bada6
redonearth <wona23@gmail.com>
2019-06-24 15:10:49 +0900

    Update AndroidJspApplication.java

--- src/main/java/com/redonearth/androidjsp/AndroidJspApplication.java
+++ src/main/java/com/redonearth/androidjsp/AndroidJspApplication.java
@@ -1,10 +1,7 @@
 package com.redonearth.androidjsp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AndroidJspApplication {
